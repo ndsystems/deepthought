8dfc3bfd2c3573e51bf47c70ce10664260dc7358
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-07-19 02:48:58 +0530

    series acquisition and imaging

--- deepthought/acquisition.py
+++ deepthought/acquisition.py
@@ -6,18 +6,19 @@ import numpy as np
 class ImageSeries:
     def __init__(self):
         self.tasks = []
+        #self.scope = Acqusition()
 
     def xy(self, position):
         print(f"xy: {position}")
-        yield "xy"
+        yield #self.scope.xy = position 
 
     def z(self, position):
         print(f"z: {position}")
-        yield "z"
+        yield #self.scope.z = position 
 
     def exp(self, exposure):
         print(f"exp: {exposure}")
-        yield "exposure"
+        yield #self.scope.exposure = exposure
 
     def xy_scan(self, positions):
         self._xy_task = lambda: (self.xy(pos) for pos in positions)
@@ -38,15 +39,23 @@ class ImageSeries:
             if m < len(self.tasks):
                 self.run(m)
 
+    def __image(self):
+        print("imaging")
+        yield
 
+    def image(self):
+        self._image = lambda: (self.__image() for _ in [None])
+        self.tasks.append(self._image)
+    
     def __repr__(self):
-        return str(self.self.tasks)
+        return str(self.tasks)
 
 
 if __name__ == "__main__":
     s = ImageSeries()
     positions = [[0, 0], [100, 100], [1000, 1000]]
-    s.z_scan([0, 100, 1000, 10000])
     s.xy_scan(positions)
+    s.z_scan([0, 100, 1000, 10000])
     s.exp_scan([50, 100, 150])
+    s.image()
     s.run(0)
