df57f13417bc5572e49a034ab05ae56661039a36
Kesavan Subburam <pskesavan@tifrh.res.in>
2020-07-19 10:57:26 +0530

    reshape images to the right dimension

--- deepthought/acquisition.py
+++ deepthought/acquisition.py
@@ -1,11 +1,12 @@
 from hardware_handler import Acqusition
-import itertools
 import numpy as np
 
 
 class ImageSeries:
     def __init__(self):
         self.tasks = []
+        self.images = []
+        self.shapes = []
         #self.scope = Acqusition()
 
     def xy(self, position):
@@ -23,24 +24,33 @@ class ImageSeries:
     def xy_scan(self, positions):
         self._xy_task = lambda: (self.xy(pos) for pos in positions)
         self.tasks.append(self._xy_task)
+        self.shapes.append(len(positions))
     
     def z_scan(self, positions):
         self._z_task = lambda: (self.z(pos) for pos in positions)
         self.tasks.append(self._z_task)
+        self.shapes.append(len(positions))
 
     def exp_scan(self, exposures):
         self._exp_task = lambda: (self.exp(e) for e in exposures)
         self.tasks.append(self._exp_task)
+        self.shapes.append(len(exposures))
 
-    def run(self, n):
+    def _run(self, n):
         for generator in self.tasks[n]():
             next(generator)
             m=n+1
             if m < len(self.tasks):
-                self.run(m)
+                self._run(m)
+                print(m)
+
+    def run(self):
+        self._run(0)
+        self.images = np.array(self.images).reshape(self.shapes)
 
     def __image(self):
         print("imaging")
+        self.images.append(1)
         yield
 
     def image(self):
@@ -58,4 +68,4 @@ if __name__ == "__main__":
     s.z_scan([0, 100, 1000, 10000])
     s.exp_scan([50, 100, 150])
     s.image()
-    s.run(0)
+    s.run()
