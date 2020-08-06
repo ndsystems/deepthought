470607cd3e535ce5fa2169e69a0faee3424c1a1f
Kesavan Subburam <pskeshu@gmail.com>
2020-08-07 01:30:46 +0530

    latest

--- deepthought/hardware_ophyd.py
+++ deepthought/hardware_ophyd.py
@@ -1,16 +1,28 @@
+import matplotlib
+matplotlib.use('qt5agg')
 from bluesky.plans import scan
 from bluesky import RunEngine
 from bluesky.utils import ProgressBarManager
 from mm_bluesky import Focus, Camera
 from comms import get_object
+from databroker import Broker
 
-mmc = get_object(port=18861).mmc
+mmc = get_object(addr="localhost", port=18861).mmc
 
 RE = RunEngine({})
+
 RE.waiting_hook = ProgressBarManager()
+db = Broker.named('temp')
+
+RE.subscribe(db.insert)
 
 z = Focus(mmc)
 cam = Camera(mmc)
 
-from ophyd.sim import det
-RE(scan([det], z, 0, 10, num=9))
+import ophyd
+# z = ophyd.sim.motor
+# cam = ophyd.sim.direct_img
+
+uid, = RE(scan([cam], z, 0, 10, num=9))
+header = db[uid]
+
