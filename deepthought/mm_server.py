4523c2438afa2df78fc84f6ed57620e4e3771c7f
Kesavan Subburam <pskeshu@gmail.com>
2020-08-14 04:51:43 +0530

    expanded fake images into Micromanager

--- deepthought/mm_server.py
+++ deepthought/mm_server.py
@@ -20,13 +20,13 @@ class Micromanager(rpyc.Service):
 
     def load_microscope(self, config_path=None):
         """initialize MMCore for the given micro-manager config file"""
-
         if config_path is None:
             config_path = config["mm_config"]
         config_abspath = os.path.abspath(config_path)
+        
         mm_dir = config["mm_dir"]
-        working_dir = os.getcwd()
 
+        working_dir = os.getcwd()
         os.chdir(mm_dir)
 
         self.mmc = pymmcore.CMMCore()
@@ -34,22 +34,20 @@ class Micromanager(rpyc.Service):
         self.mmc.loadSystemConfiguration(config_abspath)
 
         os.chdir(working_dir)
+
         return self.mmc
 
+    def get_random_crop(self, img, size=300):
+        """generate a random crop of the image for the given size"""
+        x, y = img.shape
+        random_x = np.random.randint(0, int(x/2))
+        random_y = np.random.randint(0, int(y/2))
+        cropped_img = img[random_x:random_x+size, random_y:random_y+size]
+        return cropped_img
 
-def get_random_crop(img, size=300):
-    """generate a random crop of the image for the given size"""
-
-    x, y = img.shape
-    random_x = np.random.randint(0, int(x/2))
-    random_y = np.random.randint(0, int(y/2))
-    cropped_img = img[random_x:random_x+size, random_y:random_y+size]
-    return cropped_img
-
-
-def get_simulated_image():
-    data = io.imread("data/dapi_hela.tif")
-    return get_random_crop(data)
+    def get_simulated_image(self):
+        data = io.imread("data/dapi_hela.tif")
+        return self.get_random_crop(data)
 
 
 if __name__ == "__main__":
