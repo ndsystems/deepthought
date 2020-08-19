617ad865ccdae67b796fdf3271251c1b48b8f772
Kesavan <pskesavan@tifrh.res.in>
2020-08-19 22:17:12 +0530

    simplified microscope server object

--- deepthought/configs.py
+++ deepthought/configs.py
@@ -3,10 +3,14 @@ import os
 
 config = OrderedDict()
 
-if os.name == 'nt':
-    config["mm_dir"] = "C:\Program Files\Micro-Manager-2.0gamma"
-elif os.name == "posix":
-    config["mm_dir"] = "/home/dna/lab/software/micromanager/lib/micro-manager"
+def abspath(path):
+    return os.path.abspath(path)
 
-config["mm_config"] = "./mmconfigs/demo.cfg"
+if os.name == 'nt':
+    config["mm_dir"] = abspath("C:\Program Files\Micro-Manager-2.0gamma")
+
+elif os.name == "posix":
+    config["mm_dir"] = abspath("/home/dna/lab/software/micromanager/lib/micro-manager")
+
+config["mm_config"] = abspath("./mmconfigs/Bright_Star.cfg")
 config["mm_server"] = {"addr" : "localhost", "port" : 18861}
