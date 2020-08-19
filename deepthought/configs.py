d55d59819e64b745ba92403bd94972faa9481512
Kesavan <pskesavan@tifrh.res.in>
2020-08-20 03:11:38 +0530

    refactored config

--- deepthought/configs.py
+++ deepthought/configs.py
@@ -6,29 +6,25 @@ config = OrderedDict()
 def abspath(path):
     return os.path.abspath(path)
 
+def read_env_value(name, default):
+    if name in os.environ:
+        value = os.environ[name]
+    else:
+        value = default
+    return value
 
-name = "MM_DIR"
-if name in os.environ:
-    value = os.environ[name]
-else:
-    value = "C:\Program Files\Micro-Manager-2.0gamma"
-
-config["mm_dir"] = abspath(value)
-
-
-name = "MM_CONFIG"
-if name in os.environ:
-    value = os.environ[name]
-else:
-    value = "Bright_Star.cfg"
-
-config["mm_config"] = abspath(f"./mmconfigs/{value}")
-
-
-name = "MM_IP"
-if name in os.environ:
-    value = os.environ[name]
-else:
-    value = "localhost"
-
-config["mm_server"] = {"addr" : value, "port" : 18861}
+MM_DIR = {
+            "name" : "MM_DIR", 
+            "default" : "C:\Program Files\Micro-Manager-2.0gamma"
+}
+MM_CONFIG = {
+                "name" : "MM_CONFIG",
+                "default" : "./mmconfigs/Bright_Star.cfg"
+}
+MM_SERVER = {
+            "name" : "MM_SERVER",
+            "default" : "localhost",
+}
+config["mm_dir"] = abspath(read_env_value(**MM_DIR))
+config["mm_config"] = abspath(read_env_value(**MM_CONFIG))
+config["mm_server"] = {"addr" : read_env_value(**MM_SERVER), "port" : 18861}
