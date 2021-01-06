da3747b3f517a515aa04bc43be6171c615859c8a
Kesavan <pskesavan@tifrh.res.in>
2021-01-06 19:47:10 +0530

    concept of identities

--- deepthought/sample.py
+++ deepthought/sample.py
@@ -6,8 +6,14 @@ class Form:
     """A physical object onto which the sample resides.
     
     This can be a rectangular entity, circular entity or an array entity.
+    
+    Note
+    ----
+    # talk to an architect to represent this better.
     """
-    pass
+    def __init__(self):
+        self.shape = None
+        self.size = None
 
 class Sample:
     """A sample is a multi-dimensional physical entity.
@@ -18,10 +24,11 @@ class Sample:
     
     Within these forms, collections of biological entity occupy space.
     Each sample object can therefore be identified with a xy coordinate.
-
-    
     """
     def __init__(self):
+        # who or what am I?
+        self.identity = None
+
         # a collection of SampleObjects
         self.objects = None
 
@@ -31,6 +38,24 @@ class Sample:
         # displacement upon perturbance.
         self.form = None
     
+
+class SampleIdentity:
+    """What is the identity of the sample.
+    
+        * is the sample dead or alive.
+        * is it is a cell or a tissue.
+        * what is the type of the sample.
+        * what are the conditions of the sample.
+    """
+    def __init__(self):
+        # boolean
+        self.living = None
+        # at what biological level is the sample
+        self.level = None
+        # what type of sample is it
+        self.sample_type = None
+        # under what condition does the sample exist
+        self.conditions = None
 
 
 class SampleObject:
