3a8a5dce10cdfda456caeb31d7c4654994f2bd4e
Kesavan <pskesavan@tifrh.res.in>
2021-01-06 18:57:26 +0530

    defined core objects for sample abstraction

--- deepthought/sample.py
+++ deepthought/sample.py
@@ -1,46 +1,54 @@
 """abstractions for object viewer in object dashboard"""
+from collection import OrderedDict
+
+
+class Form:
+    """A physical object onto which the sample resides.
+    
+    This can be a rectangular entity, circular entity or an array entity.
+    """
+    pass
 
 class Sample:
-    """A sample is a collection of objects, such as cells."""
+    """A sample is a multi-dimensional physical entity.
+    
+    It is materially constructed with certain design goals, onto forms
+    such as dishes or slide, each optimizing for parameters such as cost,
+    performance, sample consideration.
+    
+    Within these forms, collections of biological entity occupy space.
+    Each sample object can therefore be identified with a xy coordinate.
+
+    
+    """
     def __init__(self):
-        # ideally a collection of SampleObjects, each with properties, such as
-        # position [x, y, z], imaging parameters
+        # a collection of SampleObjects
         self.objects = None
 
-        # transformation matrix that defines the orientation of the sample
-        # objects to its own self. Useful to estimate orientation change
-        # and update this variable.
-        self.orientation = None
+        # material properties of the sample
+        # the form can have geometrical information of the sample
+        # which enables computations of factors like sample tilt, or
+        # displacement upon perturbance.
+        self.form = None
+    
 
 
 class SampleObject:
+    """An object that represents an entity in the sample."""
+    
     def __init__(self, xy):
+        # who created me?
+        self.parent = None 
+
+        # where do I look for my history?
+        self.history = None
+        
+        # where is the object in xy space of the Sample?
+        # since objects are unique in space, 
+        #   1. segmenting the raw DAPI image
+        #   2. label objects and compute regionprops
+        #   3. calculate stage coordinates for image coordinates
         self.xy = xy
 
-    def getFoci():
-        # get best z coordinate
-        pass
-
-    def getExposures():
-        # get best exposure
-        pass
-    
-    def getCameraParameters():
-        # all parameters related to MMCore Camera object - ROI, binning, gain 
-        pass
-
-    def getImage():
-        # image a range x,y,z coordinate
-        pass
-
-
-class TaskObjects:
-    """Tasks that can be run on SampleObjects"""
-    def update():
-        pass
-    
-    def timelapse():
-        pass
-
-    def monitor():
-        pass
+        # how do I image the object?
+        self.imaging_parameters = OrderedDict()        
