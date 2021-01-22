b6f1e5fcf8c2556dce0026de74d98fee207f7c05
Kesavan <pskesavan@tifrh.res.in>
2021-01-23 00:18:43 +0530

    brought this back from hard-link for now

---
+++ deepthought/configs.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict 
+import os
+
+config = OrderedDict()
+
+def abspath(path):
+    return os.path.abspath(path)
+
+def read_env_value(name, default):
+    if name in os.environ:
+        value = os.environ[name]
+    else:
+        value = default
+    return value
+
+MM_DIR = {
+            "name" : "MM_DIR", 
+            "default" : "C:\Program Files\Micro-Manager-2.0gamma"
+}
+MM_CONFIG = {
+                "name" : "MM_CONFIG",
+                "default" : "./mmconfigs/Bright_Star.cfg"
+}
+
+config["mm_dir"] = abspath(read_env_value(**MM_DIR))
+config["mm_config"] = abspath(read_env_value(**MM_CONFIG))
