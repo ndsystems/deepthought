6cd8444ab3b14bb6d56142ed41a6b15a843aed4b
Kesavan <pskesavan@tifrh.res.in>
2021-01-23 00:45:07 +0530

    cleaned up run to give live data, instead of storing tiff on disk

--- deepthought/run.py
+++ deepthought/run.py
@@ -2,7 +2,6 @@ from bluesky.plans import scan, count
 from bluesky import RunEngine
 from databroker import Broker
 from bluesky.callbacks.best_effort import BestEffortCallback
-from bluesky.callbacks.broker import LiveTiffExporter
 from devices import Camera, Focus, SimMMC
 from comms import client
 from configs import config
@@ -13,10 +12,6 @@ bec = BestEffortCallback()
 bec.disable_plots()
 
 db = Broker.named('temp')
-live = LiveTiffExporter("camera", 
-                        template="test_dir/{start[scan_id]}_{event[seq_num]}.tiff", 
-                        db=db,
-                        overwrite=True)
 
 # mmc = client(**config["mm_server"]).mmc
 mmc = SimMMC()
@@ -27,8 +22,12 @@ motor = Focus(mmc)
 RE = RunEngine({})
 RE.subscribe(bec)
 RE.subscribe(db.insert)
-RE.subscribe(live)
+RE(count([cam], num=10, delay=3))
 
-uid, = RE(count([cam], num=1))
-# header = db[uid]
-# print(header.table())
+
+# to access the data, get the header object (of databroker)
+# and access the data of camera
+header = db[-1]
+data = header.data("camera")
+for _ in data:
+    print(_)
