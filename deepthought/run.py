b43fce47a7079dec96e235bc5d14cf934c99906a
Kesavan <pskesavan@tifrh.res.in>
2021-04-19 22:12:31 +0530

    more user code

--- deepthought/run.py
+++ deepthought/run.py
@@ -1,10 +1,15 @@
 from comms import client
 from devices import SimMMC
 from microscope import Microscope
+from detection import detect_object
+from viz import imshow
 
+# https://valelab4.ucsf.edu/~MM/doc/MMCore/html/class_c_m_m_core.html
 # mmc = client(addr="10.10.1.62", port=18861).mmc
 mmc = SimMMC()
 
+# bs - bright star microscope
 bs = Microscope(mmc)
-data_header = bs.snap(num=3)
-df = data_header.table()
+images = bs.snap(num=4)
+labels = detect_object(images)
+imshow(images, labels)
