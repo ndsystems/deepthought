774a07802fcfd1b7158eecc8f8798b37ecfa32aa
Kesavan Subburam <pskesavan@tifrh.res.in>
2021-05-08 01:08:05 +0530

    #D02-3 livetiffexport of images for manual access

--- deepthought/microscope.py
+++ deepthought/microscope.py
@@ -11,11 +11,11 @@ from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
 from bluesky.plans import count, scan
 from databroker import Broker
-
+from configs import store_disk
 from devices import Camera, Focus
 
 # other devices have to be added.
-# to figure out where 
+# to figure out where
 
 
 bec = BestEffortCallback()
@@ -28,6 +28,17 @@ RE = RunEngine({})
 RE.subscribe(bec)
 RE.subscribe(db.insert)
 
+if store_disk:
+    from bluesky.callbacks.broker import LiveTiffExporter
+
+    template = "output_dir/{start[scan_id]}/{event[seq_num]}.tiff"
+    live = LiveTiffExporter("camera",
+                            template=template,
+                            db=db,
+                            overwrite=True)
+    RE.subscribe(live)
+
+
 class Microscope:
     def __init__(self, mmc):
         self.name = None
@@ -39,12 +50,12 @@ class Microscope:
     def snap(self, num=1, delay=0):
         # run a blue sky count method with cameras
         # return uid
-        uid, = RE(count(self._cam, num=num, delay=None))
-        
+        uid, = RE(count(self._cam, num=num, delay=delay))
+        print(uid)
+
         # https://nsls-ii.github.io/databroker/generated/databroker.Header.table.html#databroker.Header.table
-        header =  db[uid]
+        header = db[uid]
 
         img = np.stack(header.table()["camera"].array)
-        
-        return img
 
+        return img
