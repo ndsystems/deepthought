8d186b7d93bf53657edd42ecc68f22c66a30a1c1
Kesavan Subburam <pskesavan@tifrh.res.in>
2021-05-11 14:03:42 +0530

    cleaned up run

--- deepthought/run.py
+++ deepthought/run.py
@@ -1,19 +1,14 @@
-import sys
-import traceback
 from microscope import Microscope
 from detection import detect_object
 from viz import imshow
 
-# https://valelab4.ucsf.edu/~MM/doc/MMCore/html/class_c_m_m_core.html
-
 
 # bs - bright star microscope
 bs = Microscope()
-
-imgs = bs.scan()
-
 bs.mmc.setCameraDevice("right_port")
 
-# images = bs.snap(num=1)
-# labels=detect_object(images)
-# imshow(images)
+imgs = bs.snap()
+
+
+labels = detect_object(imgs)
+imshow(imgs, labels)
