2ea642a5608b4a015ac416b59b6c54c0dc6ae4c9
Kesavan Subburam <pskesavan@tifrh.res.in>
2021-11-30 23:46:27 +0530

    album viewer for viewing data

--- deepthought/view.py
+++ deepthought/view.py
@@ -1,24 +1,43 @@
 import napari
-import pandas
+import pandas as pd
 import numpy as np
+
 
 class AlbumViewer:
     def __init__(self, album):
         self.album = album
 
     def get_data(self):
+        dims = self.album.data.keys()
+
+        time_data = []
+
+        for dim in dims:
+            data = self.frame_set_to_df(self.album.data[dim])
+            time_data.append(data)
+
+        return pd.concat(time_data)
+
+    def frame_set_to_df(self, frame_set):
         list_of_frame_data = []
-        for frame in self.album.frames:
+        for frame in frame_set:
             list_of_frame_data.append(frame.read())
 
-        return pandas.DataFrame(list_of_frame_data)
+        return pd.DataFrame(list_of_frame_data)
 
     def view(self):
         data = self.get_data()
+
         imgs = np.stack(data["image"].to_numpy())
+        amaps = np.stack(data["anisotropy"].to_numpy())
+
+        dims = self.album.data.keys()
+        imgs = imgs.reshape(-1, len(dims), *imgs.shape[-2:])
+        amaps = amaps.reshape(-1, len(dims), *imgs.shape[-2:])
+
         v = napari.Viewer()
         v.add_image(imgs, name="image")
-        # viewer.add_image(np.stack(data["anisotropy"].to_numpy()), 
-        #                     name="anisotropy", colormap="jet", 
-        #                     contrast_limits=[0.03, 0.25])
+        v.add_image(amaps,
+                    name="anisotropy", colormap="jet",
+                    contrast_limits=[0.03, 0.25])
         v.show()
