b0d1ce542c0aea50ab680093defc3567185db44c
Kesavan Subburam <pskesavan@tifrh.res.in>
2021-12-01 10:22:51 +0530

    removed napari viewer callback to RE

--- deepthought/run.py
+++ deepthought/run.py
@@ -6,6 +6,8 @@ from bluesky.callbacks.best_effort import BestEffortCallback
 from bluesky import RunEngine
 from data import db
 from optimization import shannon_dct
+from view import AlbumViewer
+
 
 def configure_RE():
     bec = BestEffortCallback()
@@ -33,29 +35,29 @@ class ChannelConfig:
 tritc = ChannelConfig("TRITC")
 tritc.exposure = 500
 tritc.model = {"kind": "nuclei",
-                "diameter": 100}
-    
+               "diameter": 100}
+
 cy5 = ChannelConfig("Cy5")
 cy5.exposure = 1000
 cy5.model = {"kind": "nuclei",
-                "diameter": 100}
+             "diameter": 100}
 
 dapi = ChannelConfig("DAPI")
 dapi.exposure = 30
 dapi.model = {"kind": "nuclei",
-                "diameter": 100}
+              "diameter": 100}
 
 fitc = ChannelConfig("FITC")
 fitc.exposure = 500
 fitc.model = {"kind": "nuclei",
-                "diameter": 100}
-
+              "diameter": 100}
 
 
 bf = ChannelConfig("BF")
 bf.exposure = "auto"
 bf.model = {"kind": "cyto",
-                "diameter": 150}
+            "diameter": 150}
+
 
 def snap_image(mmc):
     mmc.snapImage()
@@ -64,25 +66,15 @@ def snap_image(mmc):
     print(shannon_dct(img))
     napari.view_image(img)
 
-# v = napari.Viewer()
-# layer = v.add_image(np.random.randint(0, 4095, (2048, 2048)))
-imgs = []
-def napari_viewer(event, document):
-    if event == "event":
-        if "image" in document["data"]:
-            img = document["data"]["image"]
-            imgs.append(img)
-            layer.data = np.concatenate((layer.data, img), axis=0)
 
 if __name__ == "__main__":
     scopes = MMCoreInterface()
     scopes.add("10.10.1.35", "bright_star")
     scopes.add("10.10.1.57", "eva_green")
-    
+
     m = Microscope(mmc=scopes["bright_star"])
-    
-    plan = m.scan_an_t(channels=[fitc], cycles=2, delta_t=1)
+
+    plan = m.scan_an_xy_t(channels=[fitc], num=8, cycles=26, delta_t=300)
 
     RE = configure_RE()
-    # RE.subscribe(napari_viewer)
     _ = RE(plan)
