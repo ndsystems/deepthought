7f48b392056afd6f8a3021f04a42db89302edf72
Kesavan <pskesavan@tifrh.res.in>
2021-12-07 02:37:35 +0530

    simplified view

--- deepthought/view.py
+++ deepthought/view.py
@@ -1,25 +1,8 @@
-import napari
-import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
 
-class AlbumViewer:
-    def __init__(self, album):
-        self.album = album
+def view(img):
+    plt.imshow(img)
+    plt.show()
 
-    def view(self):
-        data = self.album.get_data()
-
-        imgs = np.stack(data["image"].to_numpy())
-        amaps = np.stack(data["anisotropy"].to_numpy())
-
-        dims = len(self.album.data.keys())
-        imgs = imgs.reshape(dims, -1, *imgs.shape[-2:])
-        amaps = amaps.reshape(dims, -1, *imgs.shape[-2:])
-
-        v = napari.Viewer()
-        v.add_image(imgs, name="image")
-        v.add_image(amaps,
-                    name="anisotropy", colormap="jet",
-                    contrast_limits=[0.03, 0.25])
-        v.show()
